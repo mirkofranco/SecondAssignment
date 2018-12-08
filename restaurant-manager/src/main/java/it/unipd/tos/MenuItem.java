62454a90cea5af6e859b104369563cbc2b041f84
mirkofranco <mirko.franco@icloud.com>
2018-12-08 15:17:43 +0100

    Codifica e test classe MenuItem

---
+++ restaurant-manager/src/main/java/it/unipd/tos/MenuItem.java
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////
+// [MIRKO] [FRANCO] [1138070]
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos;
+
+
+public class MenuItem {
+    public enum itemType{
+        PIZZA,
+        PRIMI
+    }
+    private itemType type;
+    private String name;
+    private double price;
+
+    public MenuItem(itemType type, String name, double price){
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    public itemType getType(){
+        return type;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+
+}
---
+++ restaurant-manager/src/test/java/it/unipd/tos/MenuItemTest.java
@@ -0,0 +1,25 @@
+package it.unipd.tos;
+import junit.framework.TestCase;
+
+
+
+public class MenuItemTest extends TestCase{
+
+    @org.junit.Test
+    public void testGetType(){
+        MenuItem item = new MenuItem(MenuItem.itemType.PIZZA, "Margherita", 7.50);
+        assertTrue(item.getType() == MenuItem.itemType.PIZZA);
+    }
+
+    @org.junit.Test
+    public void testGetName(){
+        MenuItem item = new MenuItem(MenuItem.itemType.PIZZA, "Capricciosa", 7.50);
+        assertTrue(item.getName() == "Capricciosa");
+    }
+
+    @org.junit.Test
+    public void testGetPrice(){
+        MenuItem item = new MenuItem(MenuItem.itemType.PRIMI, "Tagliolini", 12.50);
+        assertTrue(item.getPrice() == 12.50);
+    }
+}

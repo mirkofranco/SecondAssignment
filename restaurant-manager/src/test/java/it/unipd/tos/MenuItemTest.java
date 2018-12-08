92b5d0e579a5e88be80f08a09d8ad33a9640f553
mirkofranco <mirko.franco@icloud.com>
2018-12-08 16:45:10 +0100

    Aggiornati test classe MenuItem

--- restaurant-manager/src/test/java/it/unipd/tos/MenuItemTest.java
+++ restaurant-manager/src/test/java/it/unipd/tos/MenuItemTest.java
@@ -1,26 +1,26 @@
 package it.unipd.tos;
 import it.unipd.tos.model.MenuItem;
-import junit.framework.TestCase;
+
+import static org.junit.Assert.assertEquals;
 
 
-
-public class MenuItemTest extends TestCase{
+public class MenuItemTest{
 
     @org.junit.Test
     public void testGetType(){
         MenuItem item = new MenuItem(MenuItem.itemType.PIZZA, "Margherita", 7.50);
-        assertTrue(item.getType() == MenuItem.itemType.PIZZA);
+        assertEquals(item.getType(), MenuItem.itemType.PIZZA);
     }
 
     @org.junit.Test
     public void testGetName(){
         MenuItem item = new MenuItem(MenuItem.itemType.PIZZA, "Capricciosa", 7.50);
-        assertTrue(item.getName() == "Capricciosa");
+        assertEquals(item.getName(),  "Capricciosa");
     }
 
     @org.junit.Test
     public void testGetPrice(){
         MenuItem item = new MenuItem(MenuItem.itemType.PRIMI, "Tagliolini", 12.50);
-        assertTrue(item.getPrice() == 12.50);
+        assertEquals(item.getPrice(), 12.50, 1E-10);
     }
 }

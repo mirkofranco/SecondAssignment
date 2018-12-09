955c955fed4ad38c424b1d04ae9f8da389d0fd58
mirkofranco <mirko.franco@icloud.com>
2018-12-09 16:55:29 +0100

    close #7

--- restaurant-manager/src/main/java/it/unipd/tos/business/BillMaker.java
+++ restaurant-manager/src/main/java/it/unipd/tos/business/BillMaker.java
@@ -17,6 +17,20 @@ public class BillMaker implements RestaurantBill {
             throw new RestaurantBillException("Numero di ordinazioni" +
                     " maggiori di 20");
         }
-        return 0;
+        double totalPrice = 0;
+        totalPrice = itemsOrdered.stream().mapToDouble(x -> x.getPrice()).sum();
+        if(itemsOrdered
+                .stream()
+                .filter(x -> x.getType() == MenuItem.itemType.PIZZA)
+                .count() > 10 ) {
+            totalPrice -= itemsOrdered
+                    .stream()
+                    .filter(x -> x.getType() == MenuItem.itemType.PIZZA)
+                    .mapToDouble(x -> x.getPrice()).min().getAsDouble();
+        }
+        if(totalPrice > 100 ){
+            totalPrice -= 0.05 * totalPrice ;
+        }
+        return totalPrice;
     }
 }
--- restaurant-manager/src/test/java/it/unipd/tos/BillMakerTest.java
+++ restaurant-manager/src/test/java/it/unipd/tos/BillMakerTest.java
@@ -1,11 +1,11 @@
 package it.unipd.tos;
 
 import it.unipd.tos.business.BillMaker;
-import it.unipd.tos.business.RestaurantBill;
 import it.unipd.tos.business.exception.RestaurantBillException;
 import it.unipd.tos.model.MenuItem;
 import java.util.List;
 import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 
 
 
@@ -14,12 +14,60 @@ public class BillMakerTest {
     @org.junit.Test(expected = RestaurantBillException.class)
     public void testPriceException() throws RestaurantBillException{
         List<MenuItem> item = new ArrayList<>();
-        for(int i = 0; i < 21; i++)
+        for(int i = 0; i < 21; i++) {
             item.add(new MenuItem(MenuItem.itemType.PIZZA, "Margherita", 4.50));
+        }
         BillMaker maker = new BillMaker();
         maker.getOrderPrice(item);
-
     }
 
+   @org.junit.Test
+    public void testDieciPizze(){
+        List<MenuItem> item = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            item.add(new MenuItem(MenuItem.itemType.PIZZA, "Margherita", 10));
+        }
+        item.add(new MenuItem(MenuItem.itemType.PIZZA, "Capricciosa", 9));
+        BillMaker maker = new BillMaker();
+        double totalPrice = 0;
+        try {
+            totalPrice = maker.getOrderPrice(item);
+        }catch(RestaurantBillException e){
+            System.out.println("Errore");
+        }
+        assertEquals(totalPrice, 100, 1E-100);
+    }
 
+   @org.junit.Test
+    public void testSconto(){
+        List<MenuItem> item = new ArrayList<>();
+        for(int i = 0; i < 11; i++){
+            item.add(new MenuItem(MenuItem.itemType.PRIMI, "Tagliolini", 10));
+        }
+        BillMaker maker = new BillMaker();
+        double totalPrice = 0;
+        try {
+            totalPrice = maker.getOrderPrice(item);
+        }catch(RestaurantBillException e){
+            System.out.println("Errore");
+        }
+        assertEquals(totalPrice, 104.5, 1E-100);
+    }
+
+    @org.junit.Test
+    public void testCompleto(){
+        List<MenuItem> item = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            item.add(new MenuItem(MenuItem.itemType.PIZZA, "Margherita", 20));
+        }
+        item.add(new MenuItem(MenuItem.itemType.PIZZA, "Capricciosa", 9));
+        BillMaker maker = new BillMaker();
+        double totalPrice = 0;
+        try {
+            totalPrice = maker.getOrderPrice(item);
+        }catch(RestaurantBillException e){
+            System.out.println("Errore");
+        }
+        assertEquals(totalPrice, 190, 1E-100);
+    }
 }
